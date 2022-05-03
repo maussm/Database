1169d381b1715d79ef3722f70c44a36a80cb27c1
Emile <emile@ecspresso.com>
2022-05-03 16:04:00 +0200

    Döpte om till SQL och la till drop.sql.

---
+++ sql_code_documentation/drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS
+    tk_category,
+    tk_service,
+    tk_activity_type,
+    tk_activity,
+    cost_center,
+    participant,
+    tk_alias,
+    activity,
+    activity_contents
+    CASCADE;
